1f5e205a663238a14bacab969f1f35a4d8b9fc76
Rabea Fahoum <Rabea.fahoum@gmail.com>
2020-01-19 15:13:22 +0200

    Finish step 05

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var server = http.createServer(handler);
 var message = "I am so happy to be part of the Node Girls workshop!";
 
 function handler(request, response) {
-
+    var method = request.method;
+    var endpoint = request.url;
+    console.log(endpoint);
     if (endpoint === "/") {
         response.writeHead(200, { "Content-Type": "text/html" });
-        fs.readFile(__dirname + '/public/index.html', function(error, file) {
+        fs.readFile(__dirname + '/public' + '/index.html', function(error, file) {
             if (error) {
                 console.log(error);
                 return;
             }
             response.end(file);
         });
+    } else if (endpoint === '/main.css') {
+        response.writeHead(200, { "Content-Type": "text/css" });
+        fs.readFile(__dirname + '/public/' + endpoint, function(error, file) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            response.end(file);
+        });
+    } else if (endpoint === '/img/image.jpg') {
+        response.writeHead(200, { "Content-Type": "image/jpg" });
+        fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            response.end(file);
+        });
+        //...
+    } else if (endpoint === '/favicon.ico') {
+        response.writeHead(200, { "Content-Type": "image/x-icon" });
+        fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            response.end(file);
+        });
+        // TODO - write your generic endpoint code here
     }
 }
 
