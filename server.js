60c81efc4cfd0a43cc627ccc2c09b1ab28ea756f
Rabea Fahoum <Rabea.fahoum@gmail.com>
2020-01-19 12:56:15 +0200

    finished step 04

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer(handler);
 var message = "I am so happy to be part of the Node Girls workshop!";
 
 function handler(request, response) {
 
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(message); //response body
-    response.end(); //finish response
+    if (endpoint === "/") {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile(__dirname + '/public/index.html', function(error, file) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            response.end(file);
+        });
+    }
 }
+
+// function handler(request, response) {
+//     var method = request.method;
+//     console.log(method);
+//     var endpoint = request.url;
+//     console.log(endpoint);
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     if (endpoint === "/node")
+//         response.write("/node")
+//     else if (endpoint === "/girls")
+//         response.write("/girls")
+//     else
+//         response.write(message); //response body
+
+
+//     response.end(); //finish response
+// }
 
 server.listen(3000, function() {
     console.log("server is listening on port 3000. Ready to accept requests!")
